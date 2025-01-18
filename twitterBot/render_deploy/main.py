f1a332ac9180446fc2e7019421404fac2ad5c7a9
CagatayColak <cgtycolak@gmail.com>
2025-01-18 05:28:16 +0300

    Fix error

--- twitterBot/render_deploy/main.py
+++ twitterBot/render_deploy/main.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('twitter_bot.log'),
+        logging.FileHandler('/app/logs/twitter_bot.log'),
         logging.StreamHandler()
     ]
 )
